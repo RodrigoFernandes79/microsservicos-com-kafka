3a1730f571b9a701f441b0e727deed24d7ea434a
Rodrigo Fernandes <rodrigohf79@hotmail.com>
2025-02-01 17:13:59 -0300

    criando o servico de orquestrador kafka

---
+++ servico-de-orquestrador/src/main/java/com/microsservices/servico_de_orquestrador/ServicoDeOrquestradorApplication.java
@@ -0,0 +1,13 @@
+package com.microsservices.servico_de_orquestrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoDeOrquestradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoDeOrquestradorApplication.class, args);
+	}
+
+}
